75fbcba3895ae5f4531bf8d561e7ded22eda2bda
carlohcs <carlohcs@gmail.com>
2022-06-19 00:49:12 -0300

    Module 5 - Symbol/generators/iterators references

--- 05-advanced-javascript-data-types/02-symbol.js
+++ 05-advanced-javascript-data-types/02-symbol.js
@@ -1,3 +1,15 @@
+// Refs:
+// https://www.keithcirkel.co.uk/metaprogramming-in-es6-symbols/
+// https://blog.risingstack.com/async-iterators-in-node-js/
+
+
+// Node.js code:
+// http_server.js: https://github.com/nodejs/node/blob/0d468ab200584c3aaa6ef721f51037d37c9fea7b/lib/_http_server.js#L91
+// readline.js: https://github.com/nodejs/node/blob/cb3020d82401fabc37c99fb587d951f9a8f9a6ef/lib/readline.js#L89
+// hash.js: https://github.com/nodejs/node/blob/da53a3caa306dfd31dc26fc837739571052e5c20/lib/internal/crypto/hash.js#L56
+// promises.js: https://github.com/nodejs/node/blob/8d6c2f2ada79e52ec0b376769a7d94814945bd4f/lib/internal/fs/promises.js#L78
+// inspector.js: https://github.com/nodejs/node/blob/dd5f209213a2b75bb386b44c296a059fc10dfb02/lib/inspector.js#L71
+
 const assert = require("assert")
 
 // KEYS
